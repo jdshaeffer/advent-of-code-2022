d9cc212c1f0b4751e6e11e7987082f5c02adf4de
J.D. Shaeffer <shaefferjd@gmail.com>
2022-12-01 14:53:49 -0500

    1

--- 1.py
+++ 1.py
@@ -1,43 +1,42 @@
-# find the two entries that sum to 2020
-# return their product
-'''
-- there's the quadratic solution, but that's boring
-- find the diff of each one
-- convert list into a hash map of indices and component
-    {complement: index}
-- need index too or we could just make sure that we're not comparing the same
-  one together
-- constant look up to see if the diff is there - same as google interview prep
-  question
-
-- find the 3 numbers that sum up to 2020 - there's only 1
-- take the complement dir, 
-- 2020-979=1041 so now see if there's anything in the list that could add up to
-  1041 - remove all elements that are greater than 1041 (and curr element)
-- repeat the complement dict algorithm with the diff in question being 1041
-[366, 299, 675]
-'''
-
 with open('input/1') as file:
-    input = [int(line.rstrip()) for line in file]
+    input = [int(line.rstrip()) if line != '\n' else -1 for line in file]
 
-# part1
-d = {2020-num: i for i, num in enumerate(input)}
-for i, num in enumerate(input):
-    if num in d and i != d[num]:
-        ans = num * (2020-num)
+# find the top 3
+max = 0
+second_max = 0
+third_max = 0
+curr_sum = 0
+for i,num in enumerate(input):
+    if num == -1:
+        if curr_sum > max:
+            third_max = second_max
+            second_max = max
+            max = curr_sum
+        elif curr_sum > second_max:
+            third_max = second_max
+            second_max = curr_sum
+        elif curr_sum > third_max:
+            print('cmon!!')
+            third_max = curr_sum
+        curr_sum = 0
+    else:
+        curr_sum += num
+    if i == len(input) - 1:
+        if curr_sum > max:
+            third_max = second_max
+            second_max = max
+            max = curr_sum
+        elif curr_sum > second_max:
+            third_max = second_max
+            second_max = curr_sum
+        elif curr_sum > third_max:
+            third_max = curr_sum
 
-# part2
-input = [1721, 979, 366, 299, 675, 1456]
-complement_input = [2020-num for num in input]
-for comp in complement_input:
-    remaining = [num for num in input if num < comp]
-    if len(remaining):
-        d = set()
-        d = {num for num in remaining}
-        print(input)
-        print(complement_input)
-        print(d)
-        break
+print('max', max)
+print('second_max', second_max)
+print('third_max', third_max)
+print('')
+print(sum([max, second_max, third_max]))
 
+# print(sum([max, second_max, third_max]))
 
