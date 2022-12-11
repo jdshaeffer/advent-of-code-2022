d9f6ee894ded749c8c58b2b9d586e43fda6fcd8a
J.D. Shaeffer <shaefferjd@gmail.com>
2022-12-11 14:14:12 +0100

    6 part 1, adding some extra input for catchup while traveling

---
+++ 6.py
@@ -0,0 +1,18 @@
+'''
+- find the first occurence of 4 unique chars
+- return the len of the str up to that point (at the end of the 4-char seq)
+- get windows of substrings, maintain a constant set
+- how much time complexity to init a set from a list?
+'''
+
+with open('input/6') as file:
+    input = [line.rstrip() for line in file]
+
+str = input[0]
+for i in range(len(str)-4):
+    curr = str[i:i+4]
+    set_curr = set(curr)
+    if len(curr) == len(set_curr):
+        print(i+4)
+        break
+
---
+++ 7.py
@@ -0,0 +1,2 @@
+with open ('input/7_test') as file:
+    input = []
