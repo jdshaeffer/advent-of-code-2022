3fa2296fc4511217cfc1e9555fb1ff651bbd73e0
JD Shaeffer <jshaeffer@butterflynetinc.com>
2022-12-13 11:25:01 -0500

    finish 7

--- 7.py
+++ 7.py
@@ -1,100 +1,86 @@
 from pprint import pprint
 
 '''
-- find the dirs that have at most 100_000
-- calculate the sum of their total sizes
-- good candidate for a tree of some kind - depth count
-- alternatively, init a dict for each new dir
-- append the size of each listed file
-- need to create the file structure
-- cd .. needs to know which dir it will be in
-- but there's no `cd ..` followed directly by a `$ ls` so that means i don't
-  need to know infer the location of the files, ie i don't need to know the
-  exact structure of the files
-
-- encounter a cd <dir_name>
-- init a dict with that cd
-- not elegant to encounter all sums iteratively
-
-- build the file tree, recursively do the counts using dfs
-- can have many children...
+- it's an n-ary tree
+- build it out
+- postorder traversal - need to visit the base most children first, then
+  determine the size of the parent from that - recursively
 '''
 
-# class File:
-#     def __init__(self, name, size):
-#         self.parent = None
-#         self.name = name
-#         self.size = size
+class Node:
+    def __init__(self, name, size, parent, is_dir):
+        self.parent = parent
+        self.name = name
+        self.size = size
+        self.children = []
+        self.is_dir = is_dir
 
-# class Dir:
-#     def __init__(self, name):
-#         self.parent = None
-#         self.name = name
-#         self.direct_files = {}
-#         self.dirs = {}
+class FileTree:
+    def __init__(self):
+        self.root = Node('/', 0, None, True)
 
-# class FileTree:
-#     def __init__(self):
-#         self.head = Dir('/')
+    def preorder(self):
+        def dfs(node):
+            if node:
+                res.append({'name': node.name, 'size': node.size, 'is_dir': node.is_dir})
+                for child in node.children:
+                    dfs(child)
+        res = []
+        dfs(self.root)
+        return res
 
-#     def add_dir(self, dir_name):
-#         pass
+    def cd(self, curr, name):
+        for node in curr.children:
+            if node.name == name:
+                return node
 
-#     def add_file(self):
-#         pass
+    def find_sizes(self):
+        # post order dfs (children before parent)
+        def dfs(node):
+            if node:
+                for child in node.children:
+                    dfs(child)
+                    node.size += child.size
+        dfs(self.root)
 
+with open ('input/7') as file:
+    input = [line.rstrip().split(' ') for line in file]
 
-with open ('input/7_test') as file:
-    x = [line.rstrip().split(' ') for line in file]
-
-p = 0
-c = 1
-dirs = {}
-while c < len(x):
-    if x[p][0] == x[c][0] == '$' and x[p][1] == 'cd' and x[c][1] == 'ls':
-        dir_name = x[p][-1]
-        dirs[dir_name] = {'files': 0, 'dirs': {}}
-        # jump into dir contents
-        c += 1
-        while x[c][0] != '$':
-            if x[c][0] == 'dir':
-                # add the dir names included
-                dirs[dir_name]['dirs'][x[c][-1]] = 0
+files = FileTree()
+curr = files.root
+for i in range(1, len(input)):
+    x = input[i]
+    if x[0] == '$':
+        if x[1] == 'cd':
+            cd_name = x[2]
+            if cd_name == '..':
+                curr = curr.parent
             else:
-                # only going to be files and dirs
-                file_size = int(x[c][0])
-                dirs[dir_name]['files'] += file_size
-            c += 1
-            # short circuit
-            if c >= len(x):
-                break
-    # reset
-    p = c
-    c += 1
-pprint(dirs)
+                curr = files.cd(curr, cd_name)
+        elif x[1] == 'ls':
+            parent = curr
+            i += 1
+            # jump into list of dir contents
+            while i < len(input) and input[i][0] != '$':
+                x = input[i]
+                name = x[1]
+                size = x[0] if x[0] != 'dir' else 0
+                is_dir = bool(x[0] == 'dir')
+                new_node = Node(name, int(size), parent, is_dir)
+                if curr:
+                    curr.children.append(new_node)
+                i += 1
 
-'''
-- i have the file structure
-- recursively search down each one...
-'''
+files.find_sizes()
+file_sizes = files.preorder()
+dirs = {dir['name']: dir['size'] for dir in file_sizes if dir['is_dir']}
 
-# pprint(dirs)
-# # add the remaining dirs' sums
-# # separate since can't find them in place (retroactive listing, etc)
-# for d in dirs:
-#     for od in dirs[d]['other_dirs']:
-#         dirs[d]['other_dirs'][od] = dirs[od]['files']
+# part1
+part1_dir_sizes = [file['size'] for file in file_sizes if file['is_dir'] and file['size'] <= 100_000]
+print(sum(part1_dir_sizes))
 
-
-# print('')
-# pprint(dirs)
-# # one more pass to get the nested ones
-# for d in dirs:
-#     for od in dirs[d]['other_dirs']:
-#         # add to the dirs that have their own dirs
-#         for nested_od in dirs[d]['other_dirs'][od]['other_dirs']:
-#             # but this can't handle multiple layers?
-#         # for nested_od in dirs[d]['other_dirs'][od]
-#             # dirs[d]['other_dirs'][od] += 
-
+# part2
+amount = 30000000 - (70000000 - dirs['/'])
+options = {name: size for name, size in dirs.items() if size >= amount}
+print(min([dirs[dir] for dir in options]))
 
