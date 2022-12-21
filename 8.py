d13347c533814e901944c244beff4d35ed779a37
J.D. Shaeffer <shaefferjd@gmail.com>
2022-12-21 09:25:35 -0500

    8 part 1

--- 8.py
+++ 8.py
@@ -8,14 +8,73 @@
 - visible if on an edge or if its number is greater than all others to the left of
   it or to the right of it or above it or below it
 '''
+def print_matrix(mat):
+    for i in range(len(mat)):
+        for j in range(len(mat[i])):
+            curr = mat[i][j]
+            print(mat[i][j], end='')
+        print()
+
 with open('input/8_test') as file:
     mat = [[*line.rstrip()] for line in file]
     for i in range(len(mat)):
         mat[i] = [int(digit) for digit in mat[i]]
-# perimeter = 2 * (len(mat) + len(mat[0]))
-for i in range(len(mat)):
-    for j in range(len(mat[i])):
+# -4 since it double counts corners
+perimeter = 2 * (len(mat) + len(mat[0]))-4
+
+print_matrix(mat)
+print()
+
+visible = 0
+for i in range(1, len(mat)-1):
+    for j in range(1, len(mat[i])-1):
+        # it only needs to be visible from one side
+        # need to check left, check right, check top, check bottom, all separate
         curr = mat[i][j]
-        print(mat[i][j], end='')
-    print()
+        left = top = 0
+        right = len(mat[i])-1
+        bottom = len(mat)-1
+        check = []
+
+        # left
+        while left < j:
+            check.append(mat[i][left])
+            left += 1
+        if all(l < curr for l in check):
+            visible += 1
+            continue
+        else:
+            check = []
+
+        # top
+        while top < i:
+            check.append(mat[top][j])
+            top += 1
+        if all(t < curr for t in check):
+            visible += 1
+            continue
+        else:
+            check = []
+
+        # right
+        while right > j:
+            check.append(mat[i][right])
+            right -= 1
+        if all(r < curr for r in check):
+            visible += 1
+            continue
+        else:
+            check = []
+
+        # bottom
+        while bottom > i:
+            check.append(mat[bottom][j])
+            bottom -= 1
+        if all(b < curr for b in check):
+            visible += 1
+            continue
+        else:
+            check = []
+
+print(visible + perimeter)
 
