d3718fc0e3a0e4e6850aa1e479ab15ca22417f13
J.D. Shaeffer <shaefferjd@gmail.com>
2022-12-22 10:00:29 -0500

    9 react vis attempt

---
+++ 9_react/src/App.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import { testInput, input } from './input';
+import "./App.css";
+
+function App() {
+  const rowLength = 6;
+  const rowLengthArr = [...Array(rowLength)].map(t => '.');
+
+  const [grid, setGrid] = useState(rowLengthArr.map(row => rowLengthArr))
+
+  const changeTile = (tile: string, i: number, j: number) => {
+    const newGrid = [...grid]
+    newGrid[i][j] = tile
+    setGrid(newGrid)
+  }
+
+  useEffect(() => {
+    let i = 0
+    // let head, tail = grid[5][0]
+    changeTile('H', 5, 0)
+    const intervalId = setInterval(() => {
+      // console.log(testInput[i].split(' '))
+      
+
+      i++
+      if (i === testInput.length) {
+        clearInterval(intervalId)
+      }
+    }, 1000)
+  }, [])
+
+  return (
+    <div className='App'>
+      {grid.map((_, i) => (
+        <div key={i} className='row'>
+          {grid[i].map((col, j) =>
+            <div key={`${i}, ${j}`} className='t'>
+              {col}
+            </div>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
