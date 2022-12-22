0fc66c768b172e066aca64f8bd209fda4279b7e3
JD Shaeffer <jshaeffer@butterflynetinc.com>
2022-12-22 08:18:49 -0500

    9 wip

--- 9.py
+++ 9.py
@@ -1,3 +1,14 @@
+'''
+- H and T must always be touching (top/down, left/right, diagonal, and
+  overlapping)
+- if H is 2 steps directly up, down, left or right from tail, T must move 1 step
+  in that same direction
+- if H and T are not touching, and they're not in same row or column, T must
+  move 1 step diagonally, to get closer and in the same row or column as H
+- work out where T goes as H follows the input
+- they start out in same position
+'''
+
 with open('input/9_test') as file:
     input = [line.rstrip() for line in file]
 
